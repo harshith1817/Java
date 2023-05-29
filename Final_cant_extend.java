e802aef7605468eaf1449a04d00e7b30c7003eb8
harshith1817 <127752210+harshith1817@users.noreply.github.com>
2023-05-29 18:35:52 +0530

    Week4C-3

---
+++ Final_cant_extend.java
@@ -0,0 +1,18 @@
+final class Bike1{ 
+	void run() 
+        {
+            System.out.println("final class");
+	  }
+} 
+class Honda1 extends Bike1{  
+    void run()
+        {
+            System.out.println("derived class");
+        }
+}
+class cantExtend{
+    public static void main (String args[]){
+    Honda1 obj = new Honda1();
+    obj.run();
+}
+}
